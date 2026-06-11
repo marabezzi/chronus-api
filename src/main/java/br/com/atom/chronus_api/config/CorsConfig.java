a1d6d6595fade6f14f413b5478f04e92b673593b
Guilherme Clerice <61362302+marabezzi@users.noreply.github.com>
2026-06-11 15:09:36 -0300

    chore: externaliza origins do CORS via properties

---
+++ src/main/java/br/com/atom/chronus_api/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package br.com.atom.chronus_api.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@ConfigurationProperties(prefix = "cors")
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(allowedOrigins.split(","))
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
+    }
+}
